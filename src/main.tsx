a8723618ee64aa3aed64d1e8ad5606fa6932b008
Huy070704 <he187068maithanhhuy@gmail.com>
2026-07-08 21:48:31 +0700

    shsh

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import { createRoot } from "react-dom/client";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { Analytics } from "@vercel/analytics/react";
-import App from "./App.tsx";
+import ReactGA from "react-ga4";
+import App from "./App";
 import "./styles/index.css";
 
 const GOOGLE_CLIENT_ID =
   "779479919606-hcpsuu5jij6fp6t5ams1595b80q829ko.apps.googleusercontent.com";
+
+ReactGA.initialize("G-PP39SX2K3P");
 
 createRoot(document.getElementById("root")!).render(
   <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
